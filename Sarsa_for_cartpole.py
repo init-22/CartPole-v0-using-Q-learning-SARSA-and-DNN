7633088a82dc59695df181672ac4c26890285735
Isaac <34376531+IsaacPatole@users.noreply.github.com>
2018-10-30 15:58:13 +0530

    Add files via upload

---
+++ Sarsa_for_cartpole.py
@@ -0,0 +1,89 @@
+import gym
+import numpy as np
+import math
+
+class CartPoleQAgent():
+    def __init__(self, buckets=(1, 1, 6, 12), num_episodes=1000, min_lr=0.1, min_explore=0.1, discount=1.0, decay=25):
+        self.buckets = buckets
+        self.num_episodes = num_episodes
+        self.min_lr = min_lr
+        self.min_explore = min_explore
+        self.discount = discount
+        self.decay = decay
+
+        self.env = gym.make('CartPole-v0')
+
+        # [position, velocity, angle, angular velocity]
+        self.upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50) / 1.]
+        self.lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50) / 1.]
+
+        self.Q_table = np.zeros(self.buckets + (self.env.action_space.n,))
+
+    def discretize_state(self, obs):
+        discretized = list()
+        for i in range(len(obs)):
+            scaling = (obs[i] + abs(self.lower_bounds[i])) / (self.upper_bounds[i] - self.lower_bounds[i])
+            new_obs = int(round((self.buckets[i] - 1) * scaling))
+            new_obs = min(self.buckets[i] - 1, max(0, new_obs))
+            discretized.append(new_obs)
+        return tuple(discretized)
+
+    def choose_action(self, state):
+        if (np.random.random() < self.explore_rate):
+            # randomly sample explore_rate percent of the time
+            return self.env.action_space.sample() 
+        else:
+            # take the optimal action
+            return np.argmax(self.Q_table[state])
+
+    def update_q(self, state, action, reward, new_state, new_action):
+        self.Q_table[state][action] += self.lr * ((reward + self.discount * self.Q_table[new_state][new_action]) - self.Q_table[state][action])
+
+    def get_explore_rate(self, t):
+        return max(self.min_explore, min(1., 1. - math.log10((t + 1) / self.decay)))
+
+    def get_lr(self, t):
+        return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
+
+    def train(self):
+        for e in range(self.num_episodes):
+            current_state = self.discretize_state(self.env.reset())
+
+            self.lr = self.get_lr(e)
+            self.explore_rate = self.get_explore_rate(e)
+
+            done = False
+            while not done:
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.discretize_state(obs)
+                new_action = self.choose_action(current_state)
+                self.update_q(current_state, action, reward, new_state, new_action)
+                current_state = new_state
+
+        print('Finished training!')
+
+    def run(self):
+        self.env = gym.wrappers.Monitor(self.env,'cartpole')
+        t = 0
+        done = False
+        current_state = self.discretize_state(self.env.reset())
+        while not done:
+                self.env.render()
+                t = t+1
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.discretize_state(obs)
+                current_state = new_state
+            
+        return t    
+            
+
+
+if __name__ == "__main__":
+    agent = CartPoleQAgent()
+    agent.train()
+    t = agent.run()
+    print("Time", t)
+
+
